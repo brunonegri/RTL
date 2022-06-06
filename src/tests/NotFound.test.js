d5a63457ac74173da704c273c56c255ac4b831e3
Bruno Negri <b.negri_@hotmail.com>
2022-06-06 17:26:55 -0300

    Requesito 4 feito

--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -1,1 +1,23 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes Not Found', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/NotFound');
+  });
+  test('Testa mensagem na tela "Page requested not found"', () => {
+    const noFoundText = screen.getByRole('heading', {
+      name: /page requested not found/i,
+    });
+    expect(noFoundText).toBeInTheDocument();
+  });
+  test('testa se mostra a Imagem na tela', () => {
+    const imgElement = screen.getByRole('img', {
+      name: /pikachu crying because the page requested was not found/i,
+    });
+    expect(imgElement).toHaveProperty('src', 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif');
+  });
+});
