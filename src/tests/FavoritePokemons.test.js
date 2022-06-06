a84be06830cd85201f28bcaeae4325a1d6a4a1ac
Bruno Negri <b.negri_@hotmail.com>
2022-06-06 17:14:40 -0300

    Requesito 4

--- src/tests/FavoritePokemons.test.js
+++ src/tests/FavoritePokemons.test.js
@@ -1,1 +1,16 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import App from '../App';
+import renderWithRouter from '../renderWithRouter';
+
+describe('Testes Favorites Pokemons', () => {
+  beforeEach(() => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorites');
+  });
+  test('Testa mensagem na tela "No favorite pokemon found"', () => {
+    const noFavText = screen.getByText(/no favorite pokemon found/i);
+    expect(noFavText).toBeInTheDocument();
+  });
+});
